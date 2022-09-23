81d7949f766a574768a6826e3af22701c6079a71
RandelSouza <randelsouza88@gmail.com>
2022-09-23 19:47:13 -0300

    Added backend folder

---
+++ back/controlando-leao/src/main/java/com/r/r/controlandoleao/ControlandoLeaoApplication.java
@@ -0,0 +1,13 @@
+package com.r.r.controlandoleao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlandoLeaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlandoLeaoApplication.class, args);
+	}
+
+}
